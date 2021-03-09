73b75475e7e09bd377067e633bca9b7f416703c5
weixue123 <weixue@Weis-MacBook-Pro.local>
2021-03-09 16:49:37 +0800

    Reorganize files

--- utils/data_processor.py
+++ utils/data_processor.py
@@ -1,37 +1,34 @@
+from typing import Tuple
+
 import pandas as pd
 
 from utils.indicators import *
 
-__all__ = ["build_predictors", "build_labels"]
+__all__ = ["build_predictors_and_labels"]
 
 
-def build_predictors(ohclv_data: pd.DataFrame) -> pd.DataFrame:
+def build_predictors_and_labels(ohclv_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
     """
-    For a particular asset, prepare the predictors to be input into an ML model.
-
-    :param ohclv_data:
-        A dataframe with the daily open, high, low, close and volume data of the specified asset.
-
-    :return:
-        A DataFrame of predictors.
-    """
-    predictors = _build_indicators_dataframe(ohclv_data=ohclv_data)
-    return predictors
-
-
-def build_labels(ohclv_data: pd.DataFrame) -> pd.Series:
-    """
-    For a particular asset, prepare the labels of 1's or 0's to be input into an ML model.
+    For a particular asset, prepare the predictors and labels to be input into an ML model.
 
     The output labels array will be a 1D array containing two values:
         - 1 indicating that the price of the asset rose the next day.
         - 0 indicating that the price of the asset fell the next day.
 
+    :param ohclv_data:
+        A dataframe with the daily open, high, low, close and volume data of the specified asset.
+
     :return:
-        A Series of labels.
+        A DataFrame of predictors and a Series of labels.
     """
+    ohclv_data = ohclv_data.rename(columns=str.lower)
+
+    predictors = _build_indicators_dataframe(ohclv_data=ohclv_data)
+
     daily_returns_lagged = daily_returns(close=ohclv_data["close"]).shift(-1).dropna()
-    return daily_returns_lagged.map(lambda returns: 1 if returns >= 0 else 0)
+    labels = daily_returns_lagged.map(lambda returns: 1 if returns >= 0 else 0)
+
+    return predictors, labels
 
 
 def _build_indicators_dataframe(ohclv_data: pd.DataFrame) -> pd.DataFrame:
